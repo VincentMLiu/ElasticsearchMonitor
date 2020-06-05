5ee2b4c9b280fc55aeef3f694742ff7cf7b90da5
VincentMLiu <hotwater110@hotmail.com>
2020-06-05 22:42:45 +0800

    修改开发文档

---
+++ src/main/java/com/act/test/ESJavaHighLevelClient.java
@@ -0,0 +1,13 @@
+package com.act.test;
+
+public class ESJavaHighLevelClient {
+
+
+
+    public static void main(String[] args){
+
+    }
+
+
+
+}
--- src/main/java/com/act/test/TestElasticsearch.java
+++ src/main/java/com/act/test/TestElasticsearch.java
@@ -12,8 +12,6 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TestElasticsearch {
 
@@ -23,53 +21,42 @@ public class TestElasticsearch {
     public static void main(String[] args){
 
 
-//        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-//        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("admin","admin"));
-//
-//        RestClientBuilder builder = RestClient.builder(new HttpHost("172.30.132.141", 9211))
-//                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-//                    @Override
-//                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-//                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-//                    }
-//                });
-//
-//        RestClient restClient = builder.build();
-//
-//
-//        try{
-//            Response response = restClient.performRequest("GET", "_cat/indices?v" );
-//
-//            for(Header hd : response.getHeaders()){
-//                System.out.println(hd.getName() + ": " + hd.getValue() );
-//            }
-//
-//            String responseBody = EntityUtils.toString(response.getEntity());
-//            System.out.println(responseBody);
-//
-//            restClient.close();
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        }
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("test","test"));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 19211))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestClient restClient = builder.build();
+
+
+        try{
+            Response response = restClient.performRequest("GET", "_cat/indices?v" );
+
+            for(Header hd : response.getHeaders()){
+                System.out.println(hd.getName() + ": " + hd.getValue() );
+            }
+
+            String responseBody = EntityUtils.toString(response.getEntity());
+            System.out.println(responseBody);
+
+            restClient.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
 
 
-
-//        Pattern dailyIndexPattern = Pattern.compile("(\\d{4}_(((0(1|3|5|7|8))|(1(0|2)))_(((0[1-9])|([1-2][0-9])|(3[0-1])))?)|(((0(4|6|9))|(11))_(((0[1-9])|([1-2][0-9])|(30)))?)|((02)_(((0[1-9])|(1[0-9])|(2[0-8])))?))" +
-//                "|((((\\d{2})((0[48])|([2468][048])|([13579][26]))|(((0[48])|([2468][048])|([3579][26]))00)))_02_29)");
-
-
-        Pattern dailyIndexPattern =  Pattern.compile("(\\d{4}_\\d{2}_\\d{2})|(\\d{8})");
-
-        String indexName = "test_2000_02_29";
+    }
 
 
 
-        Matcher matcher = dailyIndexPattern.matcher(indexName);
-        System.out.println(matcher.matches());
-        System.out.println(matcher.find());
-
-
+    public void testLowLevelClient(){
 
     }
 
