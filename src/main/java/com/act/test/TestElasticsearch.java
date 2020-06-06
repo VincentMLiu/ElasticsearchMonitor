1a5714b6140993110bbe7fa24085a0e94463540b
VincentMLiu <hotwater110@hotmail.com>
2020-06-06 23:31:40 +0800

    修改开发文档

--- src/main/java/com/act/test/TestElasticsearch.java
+++ src/main/java/com/act/test/TestElasticsearch.java
@@ -8,9 +8,16 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
 
 
 public class TestElasticsearch {
@@ -22,7 +29,7 @@ public class TestElasticsearch {
 
 
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("test","test"));
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("test2","s3cr37"));
 
         RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 19211))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
@@ -33,22 +40,61 @@ public class TestElasticsearch {
                 });
 
         RestClient restClient = builder.build();
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(restClient);
+
+        IndexRequest request = new IndexRequest(
+                "index2",
+                "index",
+                "1");
+        String jsonString = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
 
 
-        try{
-            Response response = restClient.performRequest("GET", "_cat/indices?v" );
+        request.source(jsonString, XContentType.JSON);
 
-            for(Header hd : response.getHeaders()){
-                System.out.println(hd.getName() + ": " + hd.getValue() );
-            }
+        try {
+            IndexResponse indexResponse = restHighLevelClient.index(request);
+            System.out.println(indexResponse.toString());
 
-            String responseBody = EntityUtils.toString(response.getEntity());
-            System.out.println(responseBody);
-
-            restClient.close();
-        }catch(Exception e){
+//            String index = indexResponse.getIndex();
+//            String type = indexResponse.getType();
+//            String id = indexResponse.getId();
+//            long version = indexResponse.getVersion();
+//            if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
+//
+//            } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
+//
+//            }
+//            ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
+//            if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
+//
+//            }
+//            if (shardInfo.getFailed() > 0) {
+//                for (ReplicationResponse.ShardInfo.Failure failure : shardInfo.getFailures()) {
+//                    String reason = failure.reason();
+//                }
+//            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        try{
+//            Response response = restClient.performRequest("GET", "_cat/indices?v" );
+//
+//            for(Header hd : response.getHeaders()){
+//                System.out.println(hd.getName() + ": " + hd.getValue() );
+//            }
+//
+//            String responseBody = EntityUtils.toString(response.getEntity());
+//            System.out.println(responseBody);
+//
+//            restClient.close();
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
 
 
 
